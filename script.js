fbda1354da81dbf05b961fe284d1eeb96f8d1390
Zoommie <zdinhsd@gmail.com>
2021-02-10 08:45:29 -0800

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+
+// Allow the user to enter their guess
+
+let num = prompt ('Guess a number between 1 and 10');
+
+// Check to see if the number is a winner
+if(num == 5){
+    alert('You guessed the right number!');
+    }else{
+// If it isn't, they got it wrongss
+        alert('You guessed the wrong number!');
+    }
